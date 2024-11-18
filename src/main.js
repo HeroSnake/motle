d38feaf9486cc655d4dccb6ea9e0281c3285ecda
Florent SYX <fsyx@vinatis.com>
2024-11-18 09:46:12 +0100

    prevent double tap zoom on IOS (dunno if it works)

--- src/main.js
+++ src/main.js
@@ -4,4 +4,37 @@ var app = new App({
 	target: document.body
 });
 
+// Prevent double-tap to zoom
+let lastTouchEnd = 0;
+	document.addEventListener(
+	'touchend',
+	(event) => {
+		const now = new Date().getTime();
+		if (now - lastTouchEnd <= 300) {
+		event.preventDefault();
+		}
+		lastTouchEnd = now;
+	},
+	false
+);
+
+// Prevent pinch-to-zoom
+document.addEventListener(
+	'touchstart',
+	(event) => {
+		if (event.touches.length > 1) {
+		event.preventDefault();
+		}
+	},
+	{ passive: false }
+);
+
+document.addEventListener(
+	'gesturestart',
+	(event) => {
+		event.preventDefault();
+	},
+	{ passive: false }
+);
+
 export default app;
